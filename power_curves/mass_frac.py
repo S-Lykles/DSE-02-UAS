b77bde2740ccfc21eea4da0c1e3bf446f638b776
S-Lykles <s.lykles@student.tudelft.nl>
2023-12-01 10:07:39 +0100

    Complete cruise and loiter calculations

--- power_curves/mass_frac.py
+++ power_curves/mass_frac.py
@@ -9,7 +9,7 @@ def Mfrac_climb():
     pass
 
 
-def Mfrac_cruise(SFC: float, R: float, CL_CD: float, eta: float) -> float:
+def Mfrac_cruise(SFC: float, R: float, CL: float, CD: float, eta: float) -> float:
     """
     Caculate fuel mass fraction of cruise phase using Breguet range equation
     R = (CL/CD) * (eta/SFC) * ln(M1/M2)
@@ -20,8 +20,10 @@ def Mfrac_cruise(SFC: float, R: float, CL_CD: float, eta: float) -> float:
         Specific fuel consumption [g/kW/h]
     R : float
         Range [km]
-    CL_CD : float
-        (optimum) Lift-to-drag ratio
+    CL : float
+        Lift coefficient should correspond to the optimum of CL/CD
+    CD : float
+        Drag coefficient
     eta : float
         Propulsive efficiency
 
@@ -31,12 +33,44 @@ def Mfrac_cruise(SFC: float, R: float, CL_CD: float, eta: float) -> float:
         Fuel mass fraction of cruise phase
     """
     R = R * 1000  # Convert range from km to m
+    SFC = SFC / 3600  # Convert SFC from g/kW/h to kg/W/s
 
-    Mfrac = np.exp(-SFC * R / (eta * CL_CD))
+    Mfrac = np.exp(-SFC * R / (eta * CL/CD))
     return Mfrac
 
 
-def Mfrac_loiter(T: float, SFC: float, ):
-    pass
+def Mfrac_loiter(E: float, SFC: float, eta, CD, CL, V1):
+    """
+    Calculate fuel mass fraction of loiter phase
+    From "Elements of airplane performance", second edition equation 15.16
+    E = 2 * eta * CL / (SFC * CD * V1) * (sqrt(W1 / W2) - 1)
+    For optimum endurance, CL and CD should correspond to maximum of CL^3/CD^2
+
+    Parameters
+    ----------
+    E : float
+        Endurance [hr]
+    SFC : float
+        Specific fuel consumption [g/kW/h]
+    eta : float
+        Propulsive efficiency
+    CD : float
+        Drag coefficient
+    CL : float
+        Lift coefficient
+    V1 : float
+        Velocity at starting phase of endurence [m/s]
+    
+    Returns
+    -------
+    Mfrac : float
+        Fuel mass fraction of loiter phase
+    """
+    E = E * 3600  # Convert endurance from hr to s
+    SFC = SFC / 3600  # Convert SFC from g/kW/h to kg/W/s
+    
+    Mfrac = (E * SFC * CD * V1 / (2 * eta * CL) + 1)**2
+    return Mfrac
+    
 
 
