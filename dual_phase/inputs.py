1a892a837d7b0443eead172565136bd1a0032d64
mahmeti <mahmeti@student.tudelft.nl>
2023-12-06 10:52:54 +0100

    First updates on functions for dual phase

--- dual_phase/inputs.py
+++ dual_phase/inputs.py
@@ -1,36 +1,62 @@
 import const #AIRCRAFT PARAMETERS
+import numpy as np
+n = 0.1
 
 M_gross = 160
 W = M_gross * const.g0 #Given in req's #Input
 
-V_max =	50 #Minimun required cruise speed * 1.3
-psi_deg	= 20 #Arbitrarily chosen
-psi_rad	= psi_deg * (np.pi / 180)
-SFC = 0.5 #Given in ppt heli design
-C_T_sig = 0.11 #Check this value later heli design ppt 18
-k = 1.1
-eff_prop = 0.7 #PLACEHOLDER
-vc = 2
+V_max =	50 #PLACEHOLDER, Minimun required cruise speed * 1.3
+V = np.arange(0,V_max+n, n)
 
-#Rotor design parameters
-DL = 230
-N = 4
-A_eq = 5 * 0.0929
+DL
+R = np.sqrt(W / (DL * np.pi))
+D = R * 2
+V_tip = 140 * D ** 0.171
+# T = from ISA
+# R = 287
+# gamma = 1.4
+# c_s = np.sqrt(gamma*R*T)
+# M_max = (V_max + V_tip)/c_s
+# if M_max<0.95
 
-#ENVIRONMENTAL PARAMETERS
-rho	= 1.2 #Arbitrarily chosen
-V_g	= 9.2 #Given in req's
 
-#AERODYNAMIC PARAMETERS
-S = 3.763 #input
-b = 6 #input
+alpha_m =
+rho =
+sigma =
+omega =
+V =
+D_v =
+k =
+v_i =
+A_eq =
 
-AR = S**2 / b #PLACEHOLDER
-e = 0.77 #PLACEHOLDER
-Cd0 = 0.02345 #PLACEHOLDER
-Cl_alpha_rot = 5.73 #Given in ppt heli design
-
-CL, CD = dragpolar(b, S, 0.2, 0.8) #From cl_cd.py
-
-#conversion factors
-HPtoWatt = 745.699872
+# psi_deg	= 20 #Arbitrarily chosen
+# psi_rad	= np.radians(psi_deg)
+# SFC = 0.5 #Given in ppt heli design
+# C_T_sig = 0.11 #Check this value later heli design ppt 18
+# k = 1.1
+# eff_prop = 0.7 #PLACEHOLDER
+# vc = 2
+#
+# #Rotor design parameters
+# DL = 230
+# N = 4
+# A_eq = 5 * 0.0929
+#
+# #ENVIRONMENTAL PARAMETERS
+# rho	= 1.2 #Arbitrarily chosen
+# V_g	= 9.2 #Given in req's
+#
+# #AERODYNAMIC PARAMETERS
+# S = 3.763 #input
+# b = 6 #input
+#
+# AR = S**2 / b #PLACEHOLDER
+# e = 0.77 #PLACEHOLDER
+# Cd0 = 0.02345 #PLACEHOLDER
+# Cl_alpha_rot = 5.73 #Given in ppt heli design
+#
+# # CL, CD = dragpolar(b, S, 0.2, 0.8) #From cl_cd.py
+#
+# #conversion factors
+# HPtoWatt = 745.699872
--- dual_phase/main.py
+++ dual_phase/main.py
@@ -1,23 +1,49 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import const as c
-import .inputs as i
+import dual_phase.inputs as i
+
+W = i.W
+R = i.R
+alpha_m = -9999
+rho = i.rho
+sigma = N_bl*c*R/(np.pi*R**2)
+
+omega = -9999
+V = i.V
+D_v = i.D_v
+k = i.k
+v_i = i.v_i
+A_eq = i.A_eq
 
 
-def power_required_rotor():
+def power_required_rotor(W, R, alpha_m, rho, sigma, omega, V, D_v, k, v_i, A_eq):
+
+
     CD_p = 0.0087 -0.0216*alpha_m+0.4*alpha_m**2
     # CD_p = 0.011 + 0.4*alpha**2
     # CD_p = 0.009 + 0.73*alpha_m**2
+
+
 
     P_phov = sigma*CD_p/8*rho*(omega*R)**3*np.pi*R**2
     mu = V/(omega*R)
     P_p = P_phov * (1+4.65*mu**2)
 
-    k_dl
+
+    k_dl = (1+D_v/W)
     T = k_dl*W
     P_i = k*T*v_i
 
-    P_req_r = P_p + P_i + P_par +P_loss
+
+    P_par = A_eq/2 * rho * V**3
+
+    P_mr = P_p + P_i + P_par
+
+    # P_loss = (3% to 6%) of P_mr
+    P_loss = 3/100*P_mr
+
+    P_req_r = P_mr +P_loss
     return P_req_r
 
 
@@ -32,3 +58,4 @@ def power_required_wing():
 def fuel_weight(SFC, P):
     W_f = SFC*P
     return W_f
+
