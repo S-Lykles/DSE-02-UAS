715ade98799c936069038906d175d88548675b5a
mahmeti <mahmeti@student.tudelft.nl>
2023-12-06 09:59:36 +0100

    New folder for dual phase

--- dual_phase/main.py
+++ dual_phase/main.py
@@ -1,1 +1,15 @@
-import const
+import const as c
+import .inputs as i
+
+
+def power_required_rotor():
+
+
+    return P_r_r
+def power_required_ring():
+    return P_r_w
+
+
+def fuel_weight(SFC, P):
+    W_f = SFC*P
+    return W_f
