041673d902f97e5283a04746b01115511108dde1
S-Lykles <s.lykles@student.tudelft.nl>
2023-12-07 12:26:24 +0100

    Add keyword to allow for battery and hydrogen

--- power_curves/mass_frac.py
+++ power_curves/mass_frac.py
@@ -91,7 +91,7 @@ import const
 #     Mfrac = (E * SFC * CD * V1 / (2 * eta * CL) + 1)**2
 #     return Mfrac
     
-def fuel_weight(CL, CD, SFC, S, which='payload', v_cruise=const.v_cruise*1.1, eta=0.75):
+def fuel_weight(CL, CD, SFC, S, which='payload', v_cruise=const.v_cruise*1.1, eta=0.75, energy_fraction=1.):
     """
     Calculate fuel weight used for endurance or payload mission
     This is only the weight for cruising and loitering, not including take-off and climb
@@ -110,6 +110,9 @@ def fuel_weight(CL, CD, SFC, S, which='payload', v_cruise=const.v_cruise*1.1, et
         'payload' or 'endurance'. The default is 'payload'.
     v_cruise : float, optional
         Cruise velocity [m/s]. The default is minimum cruise speed tines 1.1.
+    Energy_fraction : float, optional
+        Fraction fuel which is subtracted after each phase. The default is 1. In case
+        you have batteries then this would be 0, in the case of hydrogen something like 0.1
     """
     W1 = const.MTOW
     v1 = np.sqrt(W1 * 2 / (const.rho0 * S * CL))
@@ -121,7 +124,7 @@ def fuel_weight(CL, CD, SFC, S, which='payload', v_cruise=const.v_cruise*1.1, et
     E = E[idx]
     M_fuel1 = E * SFC / eta
 
-    W2 = W1 - M_fuel1 * const.g0
+    W2 = W1 - M_fuel1 * const.g0 * energy_fraction
 
     v2 = np.sqrt(W2 * 2 / (const.rho0 * S * CL))
     D = W2 * CD / CL
@@ -134,7 +137,7 @@ def fuel_weight(CL, CD, SFC, S, which='payload', v_cruise=const.v_cruise*1.1, et
     idx = np.argmin(E)
     M_fuel2 = E[idx] * SFC / eta
 
-    W3 = W2 - M_fuel2 * const.g0
+    W3 = W2 - M_fuel2 * const.g0 * energy_fraction
     
     if which == 'payload':
         W3 -= const.Payload
