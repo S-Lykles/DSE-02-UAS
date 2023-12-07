ad644f36e1aa9ad593d6dfe5887b61781d7b9fdc
S-Lykles <s.lykles@student.tudelft.nl>
2023-12-07 12:25:32 +0100

    fuel use for compound using compound drag polar

---
+++ compound/plot_fuel.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import const
+import power_curves.inputs as i
+from power_curves.mass_frac import fuel_weight
+from aero.compound_helicopter import dragpolar_heli
+
+n = 0.5
+b = np.arange(2,6+n,n)
+S = np.arange(1,5+n,n)
+print(len(b), len(S))
+
+fuel_w = np.zeros((len(b), len(S)))
+unit_conversion = 0.45359237 / 745.7/ 60**2
+SFC = 0.5 * unit_conversion
+for i in range(len(b)):
+    for j in range(len(S)):
+        CL, CD = dragpolar_heli(b[i], S[j], Cl_start=0.01)
+        fw = fuel_weight(CL, CD, SFC, S[j], which='endurance', v_cruise=const.v_cruise*1.1)
+        fuel_w[i][j] = fw
+
+
+fuel_w = pd.DataFrame(fuel_w)
+# # Create a heatmap
+plt.figure(figsize=(10, 8))
+sns.heatmap(fuel_w, annot=True, xticklabels=b, yticklabels=S, cmap='viridis')
+plt.xlabel('b')
+plt.ylabel('S')
+plt.title('Fuel weight')
+plt.show()
