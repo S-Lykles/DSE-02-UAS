b1c1d9429c740eed18f12225e7937ffa7ff5c9b2
keesnienhuis <100360406+keesnienhuis@users.noreply.github.com>
2023-12-22 17:23:07 +0100

    curve lift distribution over blade

--- DSE/Rotor_Sizing/formulas.py
+++ DSE/Rotor_Sizing/formulas.py
@@ -2,15 +2,62 @@ import numpy as np
 import pandas as pd
 import csv
 from DSE.Rotor_Sizing import *
-from DSE import *
+from DSE import const
+from pathlib import Path
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.integrate import quad
 
-csv_file_path = 'DSE/Rotor_Sizing/clarky.csv'
-data = pd.read_csv(csv_file_path)
+file_dir = Path(__file__).parent
 
-print(data)
+def data_file():
+    data = pd.read_csv(file_dir / r"clarky.csv")
 
-def lift_blade(cl, cd,  rho, omega, r, c):
-    L = (cl * rho * (omega*r)**2 * c) / 2
-    D = (cd * rho * (omega*r)**2 * c) / 2
-    RPM = (2 * np.pi) / omega
+    x_axis = 'Alpha'
+    y_axis = 'Cl'
+
+    data.plot(x=x_axis, y=y_axis, marker='', linestyle='-')
+
+    plt.title('Title')
+    plt.xlabel(x_axis)
+    plt.ylabel(y_axis)
+    plt.grid()
+    plt.show()
+
     return
+
+data_file()
+
+def lift_blade(r):
+    Cl = 0.7370
+    rho = 1.225
+    omega = (5000*2*np.pi) / 60
+    c = 0.03
+    return 0.5 * Cl * rho * (omega * r)**2 * c
+
+def drag_blade(r):
+    Cd = 0
+    rho = 1.225
+    omega = (5000 * 2 * np.pi) / 60
+    c = 0.03
+    return 0.5 * Cd * rho * (omega * r) ** 2 * c
+
+lower_limit = 0.0
+upper_limit = 0.5
+
+result, error = quad(lift_blade, lower_limit, upper_limit)
+
+print(f"Result of integration: {result}")
+print(f"Estimate of error: {error}")
+
+r_values = np.linspace(lower_limit, upper_limit, 1000)
+integrand_values = lift_blade(r_values)
+
+plt.plot(r_values, integrand_values, label='Lift')
+plt.fill_between(r_values, 0, integrand_values, alpha=0.2)
+
+plt.title('Lift distribution over blade')
+plt.xlabel('Radius [r]')
+plt.ylabel('Lift [N]')
+plt.legend()
+plt.show()
