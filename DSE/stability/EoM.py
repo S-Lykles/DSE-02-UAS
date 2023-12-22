84317530dac8812595571efa6456d1e5c039a120
Jakob <Jakob.lust1605@gmail.com>
2023-12-22 10:18:19 +0100

    Sum more matrices

--- DSE/stability/EoM.py
+++ DSE/stability/EoM.py
@@ -54,18 +54,29 @@ def rotation_matrix(theta, psy, phi, deg2rad):
                   [0,0,0,0,np.sin(r_phi)/np.cos(r_theta)]])
     return R
 
-def inertial_matrix(weight):
+def inertial_matrix(mass):
     """Yet another matrix! We blessed!
     Define inputs for class_two_cg_estimation in time"""
     I_xx, I_yy, I_zz = class_two_cg_estimation()[3]
 
-    M = np.array([[weight, 0.0, 0.0, 0.0, 0.0, 0.0],
-                  [0.0, weight, 0.0, 0.0, 0.0, 0.0],
-                  [0.0, 0.0, weight, 0.0, 0.0, 0.0],
+    M = np.array([[mass, 0.0, 0.0, 0.0, 0.0, 0.0],
+                  [0.0, mass, 0.0, 0.0, 0.0, 0.0],
+                  [0.0, 0.0, mass, 0.0, 0.0, 0.0],
                   [0.0, 0.0, 0.0, I_xx, 0.0, 0.0],
                   [0.0, 0.0, 0.0, 0.0, I_yy, 0.0],
                   [0.0, 0.0, 0.0, 0.0, 0.0, I_zz]])
     return M
 
-def gyroscopic_matrix(weight, velocity_vector):
-    """Gyroscopic matrix, """
+def gyroscopic_matrix(mass, velocity_vector):
+    """Gyroscopic matrix, Define inputs for class_two_cg_estimation, velocity vector called p in lit"""
+
+    I_xx, I_yy, I_zz = class_two_cg_estimation()[3]
+    u, v, w, p, q, r = velocity_vector
+
+    C = np.array([[0.0, -mass * r, mass * q, 0.0, 0.0, 0.0],
+                  [mass * r, 0.0, -mass * p, 0.0, 0.0, 0.0],
+                  [-mass * q, mass * p, 0.0, 0.0, 0.0, 0.0],
+                  [0.0, -mass * w, mass * v, 0.0, I_yy * r, -I_zz * q],
+                  [mass * w, 0.0, -mass * u, I_zz * r, 0.0, -I_xx * p],
+                  [-mass * v, mass * u, 0.0, I_xx * q, -I_yy * p, 0.0]])
+    return C
