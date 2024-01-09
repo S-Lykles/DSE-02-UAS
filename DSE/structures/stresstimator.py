c483b6855bff5a86380cb5e12590e40f43fb1329
DionVanStrydonck <dionavs@gmail.com>
2024-01-09 13:22:46 +0100

    v2.0 internal load model with a very, VERY messy sizing for the wing structure, which I wanted to put in stresstimator.py but would be doing a lot of double work. This code is functional, but horrific in terms of integration with other stuff, I am sorry Sieds.

---
+++ DSE/structures/stresstimator.py
@@ -0,0 +1,15 @@
+print('Structures <3 !!!')
+from DSE import const
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+
+
+
+def stress_per_section():
+
+def aMOI_required(moment_dist, allow_stress, wing_geom):
+
+
+def bending_stress(moment_dist, AMOI_dist, wing_geom)
