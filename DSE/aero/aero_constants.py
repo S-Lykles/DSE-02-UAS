5d64fdbeb98c420397c68e6435a55059ff1574de
baslieffering <b.lieffering@gmail.com>
2024-01-10 15:38:33 +0100

    vertical tail update v1.0

--- DSE/aero/aero_constants.py
+++ DSE/aero/aero_constants.py
@@ -13,6 +13,9 @@ def aero_values():
     CL_0 = cl_23012[10]
     c_root = 0.833
     c_tip = 0.333
-    return c_bar, cm_0, Cm_alpha, alpha_0, sweep_ang_25_c_rad, sweep_ang_50_c_rad, Cl_alpha_wing, CL_max, CL_0, c_root, c_tip
+    CL_cruise = cl_23012[9]
+    b = 6
+    S = 3.5
+    return c_bar, cm_0, Cm_alpha, alpha_0, sweep_ang_25_c_rad, sweep_ang_50_c_rad, Cl_alpha_wing, CL_max, CL_0, c_root, c_tip, CL_cruise, b, S
 
 print(aero_values())
