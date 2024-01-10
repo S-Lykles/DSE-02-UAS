930928458a62a96d13282ad2cb39018864fc0619
baslieffering <b.lieffering@gmail.com>
2024-01-10 14:18:54 +0100

    vertical tail update v1.0

--- DSE/stability/tail_sizing.py
+++ DSE/stability/tail_sizing.py
@@ -61,48 +61,94 @@ def horizontal_tail_sizing(S=3.5, b=6, c_bar=0.619, b_f, hf_max, l_fn, sweep_ang
 
     return Sh_S, x_np_bar, x_cg_bar,Vh_V_2
 
-def vertical_tail_size_1(tail_config='t-tail'):
-    'Sv_bv is still the coupled ratio of vertical tail span and surface area of the both sections. Sv1_bv1 is the coupled ratio of the vertical tail and span of one of the the vertical tail sections.
+def vertical_tail_size():
+    # base imports.
+    deg2rad = np.pi / 180
+    b = 6
+    S = 3.5
+    l_fus = 6
+    eta = 0.9
+    b_max = 0.7
+    Cl_alpha = 1.0  # the cl_alpha of the vertical tail at transitional velocity
+    CL = 1.3  # at transitional velocity
+    Xcg = 3.5147906877402817
+    AR_w = b ** 2 / S
 
-    Bp =    3     # number of blades of the pusher propellor
-    lp =  locations()[4]      # the height difference between propellor and c.g.
-    Dp =    2*0.5    # Diameter of the propellor
-    lf =   2     # length of the fuselage
-    hf_max = 0.9   # the maximum height of the fuselage
-    hf1 =       # height of the fuselage at 25% length
-    hf2 =       # height of the fuselage at 75% length
-    bf1 =       # width of the fuselage at 25% length
-    bf2 =       # width of the fuselage at 75% length
-    lcg =       # distance from nose to cg
-    Sfy =       # side surface area of the fuselage
+    # initial starting values
+    lv = 3
+    tail_volume = 0.055
+    C_eta_beta = 0.058
+    taper_v = 1 / 0.40
+    step_size = 50
 
-    if tail_config == 'fuselage-mounted':
-        C_eta_beta_i =  0.024
-    elif tail_config == 'fin-mounted':
-        C_eta_beta_i =  0.012
-    elif tail_config == 't-tail':
-        C_eta_beta_i = -0.017
+    # intergration space
+    AR_v = np.arange(0.5, 2, 0.1)
+    beta = 30
+    sweep_v = np.arange(0, 45, 1)
+    aa = np.linspace(0, l_fus, num=step_size)
 
-    k_beta = 0.3 * (lcg/lf) + 0.75* hf_max/lf -0.105
-    C_eta_beta_fuse = -1*k_beta * (Sfy*lf)/(S*b)* np.sqrt(hf1/hf2) * (bf2/bf1)**(1/3)
-    C_eta_beta_prop = -0.053 *Bp *(lp*Dp**2)/(S*b)
-    C_eta_beta = C_eta_beta_fuse +C_eta_beta_prop + C_eta_beta_i
-    print ('the value of C_eta_beta is:', C_eta_beta)
-    print ('In order to determine the vertical tail surface, the tail volume of the tail has to be found from the graph using C_eta_beta')
-    return C_eta_beta
+    # Empty list set
+    Surface = []
+    span = []
+    Moment_arm = []
 
-def vertical_tail_surface(V_tail_volume=0.055):
-    print('first run "vertical_tail_surface_1".')
-    lv = locations()[8]
-    S =  #aero import
-    b =  #aero import
-    c_h_tip = # import horizontal tail surface.
-    AR_v = 1.25 #current base from lit review (Sadraey, M., Aircraft Design: A Systems Engineering Approach, 2012) #import
+    Sv = tail_volume * S * b / lv
 
-    Sv = V_tail_volume *S*b/lv /2
-    cv = c_h_tip
-    bv = np.sqrt(AR_v*Sv)
-    return Sv, cv, bv
+    for p in range(len(AR_v)):
+        Surface_k = []
+        span_k = []
+        moment_arm_k = []
+        for j in range(len(sweep_v)):
+            for k in range(100):
+                bv = np.sqrt(AR_v[p] * Sv)
+
+                Cv = 2 / (1 + taper_v) * Sv / bv
+                Cv_bar = 2 / 3 * Cv * ((1 + taper_v + taper_v ** 2) / (1 + taper_v))
+
+                # Updated values
+                X_LEMAC_v = bv / 6 * ((1 + 2 * taper_v) / (1 + taper_v)) * np.tan(sweep_v[j] * deg2rad)
+                lv = 6 - Xcg - Cv + X_LEMAC_v + 0.25 * Cv_bar
+
+                # Update C_eta_beta
+                sweep_05_cord_v = sweep_v[j]  # for now a constant sweep is assumed
+                Cl_v_alpha = (Cl_alpha * AR_w) / (2 + np.sqrt(4 + (AR_v[p] * beta * deg2rad / eta) * (
+                            1 + (np.tan(sweep_05_cord_v * deg2rad) / (beta * deg2rad)) ** 2)))
+
+                C_eta_beta_w = CL ** 2 / (4 * np.pi * AR_w)  # + CL_h**2 / (4*np.pi*AR_h)* (Sh*bh) / (S*b)
+                new = (np.pi * l_fus * b_max ** 2) / 3
+                C_eta_beta_fuse = -2 / (S * b) * new
+
+                # Update tail surface
+                Sv = (C_eta_beta - C_eta_beta_fuse - C_eta_beta_w) / Cl_v_alpha * (S * b) / lv
+
+                # List of all values
+            Surface_k.append(Sv)
+            span_k.append(bv)
+            moment_arm_k.append(lv)
+        Surface.append(Surface_k)
+        span.append(span_k)
+        Moment_arm.append(moment_arm_k)
+
+    plot = False
+    if plot == True:
+        fig, (ax, ay, az) = plt.subplots(1, 3)
+        cp = ax.contourf(sweep_v, AR_v, Surface)
+        fig.colorbar(cp)  # Add a colorbar to a plot
+        ax.set_title('Vertical tail surface (Sv)')
+        ax.set_xlabel('Sweep angle vertical tail')
+        ax.set_ylabel('Aspect ratio vertical tail')
+
+        cy = ay.contourf(sweep_v, AR_v, span)
+        fig.colorbar(cy)
+        ay.set_title('Span of a single vertical tail plaine (bv)')
+        ay.set_xlabel('Sweep angle vertical tail')
+        ay.set_ylabel('Aspect ratio vertical tail')
+
+        cz = az.contourf(sweep_v, AR_v, Moment_arm)
+        fig.colorbar(cz)
+        az.set_title('Moment arm of the vertical tail plaine (lv)')
+        az.set_xlabel('Sweep angle vertical tail')
+        az.set_ylabel('Aspect ratio vertical tail')
 
 
 def elevator_surface_sizing(c_bar=0.619,Cm_0=-0.111,Cm_alpha=-0.029,alpha=0,alpha_0=0,CL_alpha_h= 0.12,bh_be=1):
