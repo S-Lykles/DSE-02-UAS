d73d435b3cafd92b32ba331fba82a76f0fa545d8
keesnienhuis <100360406+keesnienhuis@users.noreply.github.com>
2024-01-11 15:52:11 +0100

    now including vn diagram

--- DSE/structures/Vn_diagram.py
+++ DSE/structures/Vn_diagram.py
@@ -12,12 +12,16 @@ v_range = np.linspace(0, 50, 100)
 pos_lim = 3.8
 neg_lim = -1.52
 cl_max = 1.47
-W = 180*9.81
+W = 160*9.81
 s = 3.5
 b = 6
 rho0 = 1.225
-vne = 50
 vcr = 42
+vne = 1.25 * vcr
+amax = 15
+cla = np.deg2rad(0.099) #per dgree
+ucr = 15.2
+ud = 7.6
 
 def intersection(rho, s, cl, pos_lim, neg_lim):
     x = symbols('x')
@@ -34,6 +38,14 @@ def intersection(rho, s, cl, pos_lim, neg_lim):
 
     return intersection_points[1], intersection_points1[1]
 
+def delta_gust(rho, s, cl, w, u, v):
+    return ((0.5 * rho * cl * u * v ) / (w / s))
+
+gcr = delta_gust(rho0, s, cl_max, W, ucr, vcr)
+gd = delta_gust(rho0, s, cl_max, W, ud, vne)
+
+gust_limits = np.array([[0,1], [vcr, 1+gcr], [vne, 1+gd], [vne, 1-gd], [vcr, 1-gcr], [0,1]])
+
 va, va_n = intersection(rho0, s, cl_max, pos_lim, neg_lim)
 
 L = lift(rho0, v_range, s, cl_max)
@@ -43,18 +55,19 @@ n2 = -L / W
 mask = n1 <= pos_lim
 mask2 = n2 >= neg_lim
 
-y_b = [neg_lim, neg_lim, 0, pos_lim, pos_lim]
-x_b = [va_n, vcr, vne, vne, va]
+vn_limits = np.array([[va_n,neg_lim], [vcr, neg_lim], [vne, 0], [vne, pos_lim], [va, pos_lim]])
 
-plt.figure()
+plt.figure(dpi=300)
 plt.title('V-n diagram', fontsize='x-large')
 plt.plot(v_range[mask], n1[mask], color='green')
 plt.plot(v_range[mask2], n2[mask2], color='green')
-plt.plot(x_b, y_b, linestyle='-', color='green')
-#plt.plot(vne, 0, marker='x', label='Vne')
-plt.plot(vcr, 0, marker='x', label='Vcr')
-plt.plot(va, 0, marker='x', color='red', label='Va')
+plt.plot(gust_limits[:,0], gust_limits[:, 1], linestyle='-', label='Gust Envelope')
+plt.plot(vn_limits[:, 0], vn_limits[:, 1], linestyle='-', color='green', label='Vn Diagram')
+# plt.plot(vcr, 0, marker='x', label='Vcr')
+# plt.plot(va, 0, marker='x', color='red', label='Va')
 plt.xlabel('V [m/s]')
 plt.ylabel('n [-]')
 plt.legend()
+plt.savefig('Vn_diagram.png')
 plt.show()
+
