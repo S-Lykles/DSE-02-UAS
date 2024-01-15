78410d771d2925431716dfaaf353e74821f3bf77
FlorianTjepkema <80331009+FlorianTjepkema@users.noreply.github.com>
2024-01-15 17:31:24 +0100

    update derivatives

--- DSE/stability/control_derivatives.py
+++ DSE/stability/control_derivatives.py
@@ -1,13 +1,14 @@
 from DSE.aero.aero_constants import *
+# from DSE.aero.xfoil_plots import *
 from DSE.Locations import *
 from math import *
 import numpy as np
 
 CL = CL_cruise
 # initial equilibrium condition at 2 deg before cruise angle
-alpha_initial_eq = alpha_0012_airfoil[index_cruise_wing-4]*pi/180 
-CD_initial_eq = cd_23012_wing[index_cruise_wing-4] 
-CL_initial_eq = cl_23012_wing[index_cruise_wing-4]
+alpha_initial_eq = 2
+CD_initial_eq = 2
+CL_initial_eq = 2
 
 gamma0 = 2*pi/180
 
@@ -16,20 +17,20 @@ alpha = alpha_23012_wing[index_cruise_wing]*pi/180 # cruise at alpha where cl.\/
 CD = cd_23012_wing[index_cruise_wing]
 CT = 0.01
 V = 42
-V_h = V
+V_h = V # assume no interference of freestream air for horizontal tail
 T = 288.15 - 0.0065 * 500
 M0 = V/(sqrt(1.4*287.15*T))
-
-CDM = 0 # No effect on cd due to increasing Mach number
-CLM = 0 # No effect on cl due to increasing Mach number
-CLM_w = 0 # No effect on cl due to increasing Mach number
-CLM_h = 0 # No effect on cl due to increasing Mach number
+print(M0)
+CDM = CD_initial_eq * M0 / (1-M0**2) # No effect on cd due to increasing Mach number
+CLM = CL_initial_eq * M0 / (1-M0**2) # No effect on cl due to increasing Mach number
+CLM_w = CLM # No effect on cl due to increasing Mach number
+CLM_h = CLM * S_h / S # No effect on cl due to increasing Mach number
 
 CL_alpha = CL_alpha_wing
 CD_alpha = CD_alpha_wing
 CTalpha = 0 #  assumption form marilenas paper
 CT0 = CD_initial_eq + CL_initial_eq * atan(gamma0) # citation 14 from marilenas control derivatives paper
-CTu_w = -3 * CT0
+CTu_w = 0
 CTu_h = CTu_w * S_h / S # assumption using ratio of volumes
 
 l_fr, l_aft, l_acw,l_h,h_p,h_acw,h_h,z_h,X_lemac, Xcg, Zac, Zh = locations()
@@ -40,7 +41,7 @@ Zcg_w = Zac
 Zcg_h = Zh
 lw = l_h - l_acw
 S_w = S
-St =  S_w + S_h
+St =  S_w
 c = c_bar
 
 deps_dalpha = 0 # Assumption
@@ -77,13 +78,13 @@ CXde = 0
 # CZde = 
 # Cmde = 
 
-# A = [CXu, CZu, Cmu,
-#      CXalpha, CZalpha, 0,
-#      CXq, CZq, Cmq,
-#      CXalphadott, CZalphadott, 0,]
-# print("""
-# CXu, CZu, Cmu,
-# CXalpha, CZalpha, 0,
-# CXq, CZq, Cmq,
-# CXalphadott, CZalphadott, 0
-# """, A)
+A = np.array([[CXu, CZu, Cmu],
+     [CXalpha, CZalpha, 0],
+     [CXq, CZq, Cmq],
+     [CXalphadott, CZalphadott, 0]])
+print("""
+          CXu, CZu, Cmu,
+          CXalpha, CZalpha, 0,
+          CXq, CZq, Cmq,
+          CXalphadott, CZalphadott, 0
+""",A)
