84ba718c5d363456408696dd6ca94b09521cad5e
oramboer <oramboer@student.tudelft.nl>
2024-01-17 13:54:49 +0100

    Final boom and fuselage simulators

--- DSE/structures/Onnicomb simulator.py
+++ DSE/structures/Onnicomb simulator.py
@@ -1,6 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from DSE import plot_setting
 
+def place_booms(N_bottom, N_curve, R):
+    """
+    Distributes points equidistantly along the fuselage crossection
+    :param N_bottom: number of points on the bottom plate
+    :param N_curve: number of points along the curved section
+    :param R: outer radius
+    :return boom_co_arr: array with the coordinate pairs of each
+    """
+
+    #angles = np.linspace(np.pi/(N_curve-1), np.pi*(N_curve-2)/(N_curve-1), N_curve-2) #THIS IS OLD CODE THAT WORKED
+    angles = np.linspace(0, np.pi, N_curve)
+    curve_boom_co_arr = np.column_stack((R * np.cos(angles), R * np.sin(angles)))
+
+    bottom_boom_xco = np.linspace(-R, R, N_bottom)
+    bottom_boom_yco = np.zeros(N_bottom)
+    bottom_boom_co_arr = np.column_stack((bottom_boom_xco, bottom_boom_yco))
+
+    boom_co_arr = np.vstack((curve_boom_co_arr, bottom_boom_co_arr))
+    return boom_co_arr
 
 def calculate_centroid(R, r, t):
     """
@@ -14,9 +34,9 @@ def calculate_centroid(R, r, t):
     :return A_halftube: area of the open half tube (without bottom plate)
     :return A_bottom: area of the bottom plate
     """
-    Cy_halftube = 4/3/np.pi * (R**3-r**3) / (R**2-r**2)
-    Cy_bottom = -t/2
-    A_halftube = np.pi*(R**2-r**2) / 2
+    Cy_halftube = 2*R/np.pi
+    Cy_bottom = 0
+    A_halftube = np.pi*R*t
     A_bottom = 2*R*t
     Cy = (Cy_halftube*A_halftube + Cy_bottom*A_bottom) / (A_halftube+A_bottom)
     return Cy, Cy_halftube, Cy_bottom, A_halftube, A_bottom
@@ -33,11 +53,11 @@ def calculate_inertia(R, r, t):
     """
     Cy, Cy_halftube, Cy_bottom, A_halftube, A_bottom = calculate_centroid(R, r, t)
 
-    Ixx_halftube = np.pi*(R**4-r**4)/8 + A_halftube*Cy**2
-    Ixx_bottom = 2*R*t**3/12 + A_bottom*(Cy-Cy_bottom)**2
+    Ixx_halftube = np.pi*R**3*t/2 - A_halftube*Cy**2
+    Ixx_bottom = A_bottom*Cy**2
     Ixx = Ixx_halftube + Ixx_bottom
 
-    Iyy_halftube = np.pi*(R**4-r**4)/8
+    Iyy_halftube = np.pi*R**3*t/2
     Iyy_bottom = (2*R)**3*t/12
     Iyy = Iyy_halftube + Iyy_bottom
 
@@ -66,10 +86,10 @@ def calculate_torsion_stress(boom_co_arr, T, r, t):
     :return:
     """
 
-    stress_shear = T / (np.pi*r**2*t) * np.ones(len(boom_co_arr))
-    return stress_shear
+    stress_torsion = T / (np.pi*r**2*t) * np.ones(len(boom_co_arr))
+    return stress_torsion
 
-def calculate_shear_stress(Ixx, Iyy, Sx, Sy, N_curve, N_bottom, r):
+def calculate_shear_flow(Ixx, Iyy, Sx, Sy, N_curve, N_bottom, r):
 
     """
     Calculates the shear stress in the crossection
@@ -83,127 +103,72 @@ def calculate_shear_stress(Ixx, Iyy, Sx, Sy, N_curve, N_bottom, r):
     :return stress_shear: array with the shear stresses which corresponds to the boom placement function
     """
 
-    theta = np.linspace(0, np.pi / 2, int(N_curve/2))
+    theta = np.linspace(0, np.pi/2, int(N_curve/2))
     s2 = np.linspace(0, r, int(N_bottom/2))
-    stress12 = t * r * (Sx * r / Iyy * (np.cos(theta) - 1) + Sy / Ixx * (r * np.sin(theta) - Cy * theta))
-    stress2 = stress12[-1]
-    stress23 = -Sx * t / Iyy * (r * s2 - s2 ** 2 / 2) - Sy * t / Ixx * Cy * s2 + stress2
+    shear12 = t * r * (Sy / Ixx * (r * np.sin(theta) - Cy * theta))
+    shear2 = shear12[-1]
+    shear23 =  - Sy * t / Ixx * Cy * s2 + shear2
 
-    shear_flow = np.hstack((stress12[::-1], stress12, stress23, stress23[::-1]))
+    shear_flow = np.hstack((shear12[::-1], shear12, shear23, shear23[::-1]))
 
     return shear_flow
 
-def place_booms(N_bottom, N_curve, R):
-    """
-    Distributes points equidistantly along the fuselage crossection
-    :param N_bottom: number of points on the bottom plate
-    :param N_curve: number of points along the curved section
-    :param R: outer radius
-    :return boom_co_arr: array with the coordinate pairs of each
-    """
+#THIS FUNCTION DOESN'T WORK PROPERLY, DON'T USE!
+def better_shear_flow(Ixx, Sy, N_curve, N_bottom, R, r, Cy):
+    theta = np.linspace(0, np.pi/2, int(N_curve/2))
+    Qcurve = (R*np.sin(theta/2)/(theta/2)*np.cos(theta/2) - Cy)*(theta**R*t)
+    qcurve = Sy/Ixx*Qcurve
+    x = np.linspace(R, 0, int(N_bottom/2))
+    Qbottom = -Cy*(R-x)*t + (R*np.sin(np.pi/2)/(np.pi/2)*np.cos(np.pi/4)-Cy)*np.pi/2*R*t
+    qbottom = 0.5*Sy/Ixx*Qbottom
+    print(Qbottom)
+    shear_flow = np.hstack((qcurve[::-1], qcurve, qbottom, qbottom[::-1]))
 
-    #angles = np.linspace(np.pi/(N_curve-1), np.pi*(N_curve-2)/(N_curve-1), N_curve-2) #THIS IS OLD CODE THAT WORKED
-    angles = np.linspace(0, np.pi, N_curve)
-    curve_boom_co_arr = np.column_stack((R * np.cos(angles), R * np.sin(angles)))
+    return shear_flow
 
-    bottom_boom_xco = np.linspace(-R, R, N_bottom)
-    bottom_boom_yco = np.zeros(N_bottom)
-    bottom_boom_co_arr = np.column_stack((bottom_boom_xco, bottom_boom_yco))
 
-    boom_co_arr = np.vstack((curve_boom_co_arr, bottom_boom_co_arr))
-    return boom_co_arr
+
 
 
 if __name__ == '__main__':
+
     #INPUTS
-    R = 0.4 #m
-    t = 0.001 #m
+    N_points_bottom = 100     #even number
+    N_points_curve = 500    #even number
+    R = 0.125 #m
+    t = 0.025 #m
     r = R-t #m
-    Mx = 1943 #Nm
-    My = 0 #Nm
+    Mx = 10000 #Nm
+    My = 10000 #Nm
     Sx = 0 #N
-    Sy = 3678 #N
+    Sy = 200000 #N
     T = 0 #Nm
 
-    #the N_points have to be even numbers for this to work
-    N_points_bottom = 100
-    N_points_curve = 500
 
+    cross_sec = place_booms(N_points_bottom, N_points_curve, R)
     Ixx, Iyy, Cy = calculate_inertia(R, r, t)
-    boom_co_arr = place_booms(N_points_bottom, N_points_curve, R)
 
-    stress_bending = calculate_bending_stress(Ixx, Iyy, Cy, boom_co_arr, Mx, My) #Pa
-    stress_torsion = calculate_torsion_stress(boom_co_arr, T, r, t)
-    stress_shear = calculate_shear_stress(Ixx, Iyy, Sx, Sy, N_points_curve, N_points_bottom, R)/t
-
-    plot_stresses = True
-    plot_shear = True
-    plot_bending = False
-    plot_torsion = False
-    print('maximum shear stress = ', max(stress_shear/1000000), 'MPa')
-    print('maximum bending stress = ', max(stress_bending/100000), 'MPa')
-
-    if plot_stresses == True:
-        if plot_shear == True:
-            plt.plot([-1, +1], [Cy, Cy], label='Neutral Axis', color='black')
-            plt.scatter(0, Cy, marker='x', label='Centroid', color='black')
-            plt.legend(loc = 'upper right')
-
-            plt.scatter(boom_co_arr[:, 0], boom_co_arr[:, 1], c=stress_shear, cmap='viridis', label='Shear Stress')
-            cbar = plt.colorbar()
-            cbar.set_label('Stress [Pa]')
-
-            plt.xlabel('X-coordinate [m]')
-            plt.xlim(-0.45, 0.45)
-            plt.ylabel('Y-coordinate [m]')
-            plt.title('Shear Stress')
-            plt.grid()
-
-            # Make x and y-axis use same spacing
-            ax = plt.gca()
-            ax.set_aspect('equal', adjustable='box')
-
-            plt.show()
+    bending = calculate_bending_stress(Ixx, Iyy, Cy, cross_sec, Mx, My) #Pa
+    torsion = calculate_torsion_stress(cross_sec, T, r, t)
+    shear_flow = calculate_shear_flow(Ixx, Iyy, Sx, Sy, N_points_curve, N_points_bottom, R)
+    shear = shear_flow/t
+    shear_flow_2 = better_shear_flow(Ixx, Sy, N_points_curve, N_points_bottom, R, r, Cy)
 
 
-        if plot_bending == True:
+    plot = True
+    if plot == True:
+        plt.rcParams.update(plot_setting.report_fast)
+        plt.scatter(cross_sec[:, 0], cross_sec[:, 1], c=shear)
+        cbar = plt.colorbar()
+        plt.plot([-R-R/5, R+R/5], [Cy,Cy], label='neutral axis', color='black')
+        plt.scatter(0, Cy, marker='x', color='black', label='centroid')
+        cbar.set_label('Stress [Pa]')
+        plt.xlabel('x-coordinate')
+        plt.xlim(-R-R/5, R+R/5)
+        plt.ylabel('y-coordinate')
+        plt.grid()
+        plt.legend(loc = 'upper right')
 
-            plt.scatter(0, Cy, marker='+', label='Centroid')
-            plt.legend(loc = 'upper right')
-
-            plt.scatter(boom_co_arr[:,0], boom_co_arr[:,1], c=stress_bending, cmap='viridis', label='Bending Stress')
-            cbar = plt.colorbar()
-            cbar.set_label('Stress [units TBD]')
-
-            plt.xlabel('X-coordinate [m]')
-            plt.ylabel('Y-coordinate [m]')
-            plt.title('Bending Stress')
-            plt.grid()
-
-            #Make x and y-axis use same spacing
-            ax = plt.gca()
-            ax.set_aspect('equal', adjustable='box')
-
-            plt.show()
-
-        if plot_torsion == True:
-            plt.scatter(0, Cy, marker = '+', label = 'Centroid')
-            plt.legend(loc = 'upper right')
-
-            plt.scatter(boom_co_arr[:,0], boom_co_arr[:,1], c=stress_torsion, cmap='viridis', label='Torsion Stress')
-            cbar = plt.colorbar()
-            cbar.set_label('Stress [Pa]')
-
-            plt.xlabel('X-coordinate [m]')
-            plt.ylabel('Y-coordinate [m]')
-            plt.title('Torsion Stress')
-            plt.grid()
-
-            #Make x and y-axis use same spacing
-            ax = plt.gca()
-            ax.set_aspect('equal', adjustable='box')
-
-            plt.show()
-
-
-
+        ax = plt.gca()
+        ax.set_aspect('equal', adjustable='box')
+        plt.show()
